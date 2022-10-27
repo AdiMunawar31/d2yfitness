34ba55dcc66e41e164b3e2fcf0abea63be9bbe82
Adi Munawar <munawar.adi31@gmail.com>
2022-10-27 15:55:50 +0700

    Initial Commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Box } from '@mui/material';
+import Navbar from '../components/Navbar';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<Box width='400px' sx={{ width: { xl: '1488px' } }} m='auto'>
+			<Navbar />
+			<Component {...pageProps} />
+		</Box>
+	);
 }
 
-export default MyApp
+export default MyApp;

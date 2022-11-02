353645be03a86628f772468e16dfc25cd9f7f9eb
Adi Munawar <munawar.adi31@gmail.com>
2022-11-02 21:20:57 +0700

    create SearchExercises component

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,13 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import HeroBanner from '../components/HeroBanner';
+import SearchExercises from '../components/SearchExercises';
 
 const Home: NextPage = () => {
 	return (
 		<Box>
 			<HeroBanner />
+			<SearchExercises />
 			<h1>Hello</h1>
 		</Box>
 	);
